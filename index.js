33d9199e918d82f2891d82163b474645631747c4
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-07 14:38:23 -0500

    connected mongo db

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const keys = require('./config/keys');
+
+const mongoose = require('mongoose');
+mongoose.connect(keys.mongoUri);
 
 app.get('/', (req,res) => {
   res.send('hi there');
 })
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
