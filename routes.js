8402f9d16b7c74f6e1b4eae588c962a1d284d57b
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-09 07:40:07 -0500

    added battle api route, redux stuff

--- routes.js
+++ routes.js
@@ -1,5 +1,15 @@
+const mongoose = require('mongoose');
+const Character = mongoose.model('characters');
+
 module.exports = app => {
   app.get('/', (req,res) => {
     res.send('hi there');
+  });
+
+  app.get('/api/battle', async (req,res) => {
+    let character = await Character.aggregate(
+      { $sample: { size: 2 } }
+    )
+    res.send(character);
   });
 }
