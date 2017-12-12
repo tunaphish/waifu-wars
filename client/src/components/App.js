e65b9216905851dcd5593303eef25fc6039f4a7c
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-12 18:22:02 -0500

    added placeholder routes in app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <div>Header</div>;
+const Footer = () => <div>Footer</div>;
+const Landing = () => <div>Landing</div>;
+const About = () => <div>About</div>;
+const DivisionList = () => <div>Division List</div>;
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hey Yall
+        <BrowserRouter>
+          <div>
+            <Header/>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/division/list' component={DivisionList} />
+            <Footer/>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
