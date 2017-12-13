fcf63c4f227cc03a95e8014a1804203dd0246988
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-13 14:43:34 -0500

    added header skeleton

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Header = () => <div>Header</div>;
+import Header from './Header';
 const Footer = () => <div>Footer</div>;
 const Landing = () => <div>Landing</div>;
 const About = () => <div>About</div>;
-const DivisionList = () => <div>Division List</div>;
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <BrowserRouter>
           <div>
-            <Header/>
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/division/list' component={DivisionList} />
+            <Header />
+            <Switch>
+              <Route exact path='/about' component={About} />
+              <Route exact path='/' component={Landing} />
+            </Switch>
             <Footer/>
           </div>
         </BrowserRouter>
---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <ul>
+            <li><Link to='/'>Landing</Link></li>
+            <li><Link to='/about'>About</Link></li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header;
