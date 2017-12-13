a118e46bf5ce40bf2f0aa631fb8a5ace2a87df77
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-13 15:45:59 -0500

    added landing page cards

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -8,16 +8,41 @@ class Landing extends Component {
   }
 
   render() {
+    if (this.props.battle.length === 0) {
+      return <div>Loading</div>;
+    }
+
     return (
-      <div>
-        LANDING PAGE SON!!!!
+      <div className='row'>
+        <div className='col sm12 m5'>
+          <div className='card small'>
+            <div className="card-image">
+              <img src={process.env.PUBLIC_URL + '/characters/' + this.props.battle[0].picture }/>
+            </div>
+            <div className='card-content'>
+              {this.props.battle[0].waifuName}
+            </div>
+          </div>
+        </div>
+        <div className='col sm12 m2'>
+         VS
+        </div>
+        <div className='col sm12 m5'>
+          <div className='card small'>
+            <div className="card-image">
+              <img src={process.env.PUBLIC_URL + '/characters/' + this.props.battle[1].picture }/>
+            </div>
+            <div className='card-content'>
+              {this.props.battle[1].waifuName}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 function mapStateToProps({ battle }) {
-  console.log(battle);
   return { battle };
 }
 
