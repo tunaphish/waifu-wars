b1a390b739a946d001c8036eac25b0cca5146121
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-16 18:55:42 -0500

    added production model, changed site title, adding possible fix to prod bug

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.use(bodyParser.json());
 const mysql = require('mysql2/promise');
 mysql.createConnection(keys).then(db => {
   require('./routes')(app,db)
-});
 
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+  if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
