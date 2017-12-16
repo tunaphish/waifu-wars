2e8b8026611e499076994885b330f0b566c96aa9
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-16 16:21:54 -0500

    added ranking link to landing page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,8 +5,11 @@ class Header extends Component {
   render() {
     let divisionLink;
     if (this.props.location.pathname) {
+      if (this.props.location.pathname === '/') {
+        divisionLink = <li><Link to={'/division/1/ranking'}><i className="fa fa-star-o" aria-hidden="true"></i></Link></li>
+      }
       if (this.props.location.pathname.includes('battle')) {
-        divisionLink = <li><Link to={this.props.location.pathname.replace('battle', 'ranking')}><i className="fa fa-star-o" aria-hidden="true"></i></Link></li>
+        divisionLink = <li><Link to={this.props.location.pathname.replace('battle', 'ranking')}><i className="fa fa-star-o" aria-hidden="true"></i></Link></li>;
       }
       if (this.props.location.pathname.includes('ranking')) {
         divisionLink = <li className='versus'><Link to={this.props.location.pathname.replace('ranking', 'battle')}>VS</Link></li>
