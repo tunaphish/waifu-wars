53cdad2837525013c4bec1ac1cfaf88cdc33f6fa
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-16 16:42:38 -0500

    serve images from express back end instead of public react folder

--- client/src/components/Waifu.js
+++ client/src/components/Waifu.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default ({ name, picture }) => {
   //Only usage of inline styling because background image needs to be dynamic
   //Otherwise majority of styling uses materialize css or index.css in root src folder
-  const imageUrl = process.env.PUBLIC_URL + '/characters/' + picture;
+  const imageUrl = '/images/' + picture;
+  console.log(imageUrl);
   const style = {
     backgroundImage: `url(${imageUrl})`,
     backgroundRepeat: 'no-repeat',
