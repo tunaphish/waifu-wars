595c5b96fa7ee05a42fa52cfa1ed3723c705b3f4
tunaphish <tuanpham37@knights.ucf.edu>
2017-12-16 12:23:59 -0500

    style changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,10 +6,10 @@ class Header extends Component {
     let divisionLink;
     if (this.props.location.pathname) {
       if (this.props.location.pathname.includes('battle')) {
-        divisionLink = <li><Link to={this.props.location.pathname.replace('battle', 'ranking')}>RANKING</Link></li>
+        divisionLink = <li><Link to={this.props.location.pathname.replace('battle', 'ranking')}><i class="fa fa-star-o" aria-hidden="true"></i></Link></li>
       }
       if (this.props.location.pathname.includes('ranking')) {
-        divisionLink = <li><Link to={this.props.location.pathname.replace('ranking', 'battle')}>BATTLE</Link></li>
+        divisionLink = <li className='versus'><Link to={this.props.location.pathname.replace('ranking', 'battle')}>VS</Link></li>
       }
     }
 
@@ -17,7 +17,7 @@ class Header extends Component {
       <nav>
         <div className='nav-wrapper flow-text'>
           <ul>
-            <li><Link to='/division/list'>Division List</Link></li>
+            <li><Link to='/division/list'><i class="fa fa-bars" aria-hidden="true"></i></Link></li>
             {divisionLink}
           </ul>
           <div className='right brand-logo'><Link to='/'>WAIFU WARS</Link></div>
